86262553cdd34509b9564454dca13731ba67842a
WebDevelopmente <digbijaynayak522@gmail.com>
2022-06-13 09:23:41 +0530

    Factorial Number

--- Registration_Form/script.js
+++ Registration_Form/script.js
@@ -1,26 +1,11 @@
-$(document).ready(function () {
-  $("#btn").click(function (e) {
-    var jsonData = {};
-
-    var formData = $("#myform").serializeArray();
-    // console.log(formData);
-
-    $.each(formData, function () {
-      if (jsonData[this.name]) {
-        if (!jsonData[this.name].push) {
-          jsonData[this.name] = [jsonData[this.name]];
-        }
-        jsonData[this.name].push(this.value || "");
-      } else {
-        jsonData[this.name] = this.value || "";
-      }
-    });
-    console.log(jsonData);
-    $.ajax({
-      url: "action.php",
-      type: "POST",
-      data: jsonData,
-    });
-    e.preventDefault();
-  });
-});
+function Factorial() {
+  var number = document.getElementById("number").value;
+  var output = document.getElementById("output");
+  var i, fact;
+  fact = 1;
+  for (i = 1; i <= number; i++) {
+    fact = fact * i;
+  }
+  console.log(fact);
+  output.innerHTML = `<p>The Factorial of ${number} is: ${fact}</p>`;
+}
