cd66abe5a1140d03b238b082de44d2d8837acd55
WebDevelopmente <digbijaynayak522@gmail.com>
2022-06-14 05:11:43 +0530

    Code Changes

--- Callback_Function/script.js
+++ Callback_Function/script.js
@@ -1,11 +1,11 @@
-function multiply(a, b, callback) {
-  document.write(`The Multiplication of ${a} and ${b} is ${a * b}.` + "<br>");
-  callback();
-}
+// function multiply(a, b, callback) {
+//   document.write(`The Multiplication of ${a} and ${b} is ${a * b}.` + "<br>");
+//   callback();
+// }
 
-function disp() {
-  document.write("This must be printed after Multiplication");
-}
+// function disp() {
+//   document.write("This must be printed after Multiplication");
+// }
 
-// Calling multiply() function
-multiply(5, 6, disp);
+// // Calling multiply() function
+// multiply(5, 6, disp);
