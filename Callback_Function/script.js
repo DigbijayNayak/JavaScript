578eed0c55a4887db5e4f7099d4176d22a90255f
WebDevelopmente <digbijaynayak522@gmail.com>
2022-06-14 06:36:24 +0530

    Another Example

--- Callback_Function/script.js
+++ Callback_Function/script.js
@@ -1,3 +1,14 @@
+function greeting(name) {
+  alert("Hello " + name);
+}
+
+function processUserInput(callback) {
+  var name = prompt("Please enter your name.");
+  callback(name);
+}
+
+processUserInput(greeting);
+
 // function multiply(a, b, callback) {
 //   document.write(`The Multiplication of ${a} and ${b} is ${a * b}.` + "<br>");
 //   callback();
@@ -9,3 +20,14 @@
 
 // // Calling multiply() function
 // multiply(5, 6, disp);
+
+// Another Example
+// setInterval(myFunction, 1000);
+
+// function myFunction() {
+//   let d = new Date();
+//   document.getElementById("demo").innerHTML=
+//   d.getHours() + ":" +
+//   d.getMinutes() + ":" +
+//   d.getSeconds();
+// }
