48e945912198cb00801c44477865dd9c5195b63e
WebDevelopmente <digbijaynayak522@gmail.com>
2022-06-15 22:59:26 +0530

    Completed Callback

--- Callback_Function/script.js
+++ Callback_Function/script.js
@@ -8,26 +8,3 @@ function processUserInput(callback) {
 }
 
 processUserInput(greeting);
-
-// function multiply(a, b, callback) {
-//   document.write(`The Multiplication of ${a} and ${b} is ${a * b}.` + "<br>");
-//   callback();
-// }
-
-// function disp() {
-//   document.write("This must be printed after Multiplication");
-// }
-
-// // Calling multiply() function
-// multiply(5, 6, disp);
-
-// Another Example
-// setInterval(myFunction, 1000);
-
-// function myFunction() {
-//   let d = new Date();
-//   document.getElementById("demo").innerHTML=
-//   d.getHours() + ":" +
-//   d.getMinutes() + ":" +
-//   d.getSeconds();
-// }
