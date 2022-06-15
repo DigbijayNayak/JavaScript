e91dca0829be9197d27266e044a632603d0c42fa
Digbijay Nayak <digbijaynayak522@gmail.com>
2022-06-15 10:17:21 +0530

    Deleted Time Folder

--- Timezone/script.js
+++
@@ -1,8 +0,0 @@
-
-function disp_TZ(){
-    var tz=document.getElementById("timezone").value;
-    var str= new Date().toLocaleString("en-US", 
-        {timeZone: tz,timeZoneName:"short"})
-    document.getElementById('tresult').innerHTML = str + "," + tz;
-    // display_c()
-}
